a529bd746dc20aceba2cc3d56eff81b4bf4c936a
neofluar <you@example.com>
2020-02-01 11:16:04 +0300

    Delete 3.8-specific f-string

--- my_balance.py
+++ my_balance.py
@@ -7,10 +7,10 @@ class App:
         self.init_data = InitData()
 
     def run_app(self):
-        print(f'{self.init_data.YEAR=}')
-        print(f'{self.init_data.MONTHS=}')
-        print(f'{self.init_data.CATEGORIES=}')
-        print(f'{self.init_data.default_month()=}')
+        print(f'{self.init_data.YEAR}')
+        print(f'{self.init_data.MONTHS}')
+        print(f'{self.init_data.CATEGORIES}')
+        print(f'{self.init_data.default_month()}')
 
 
 if __name__ == '__main__':
